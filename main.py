7f60710d26da673f07ceae8a6c22095ca6b31d9d
Sadfahlsdj <124646333+Sadfahlsdj@users.noreply.github.com>
2023-12-22 13:59:38 -0800

    starting the scraper

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 import requests
 import json
-
-api_token = "is this needed"
+from bs4 import BeautifulSoup
 
 # replace the start value with something else to access a different entry
 # retformat can be pretty, json, php, or wddx
 api_url = ("https://imslp.org/imslpscripts/API.ISCR.php?account=worklist/disclaimer=accepted/"
            "sort=id/type=2/start=0/retformat=json")
-
-api_url_test = ("http://imslp.org/imslpscripts/API.ISCR.php?account=worklist/disclaimer=accepted"
-                "/sort=id/type=1/start=0/retformat=<pretty|json|php|wddx>")
 
 bad_query_message = "response code was not 200, something went 2 shit"
 
@@ -19,7 +15,7 @@ def get_music():
     url = api_url
     response = requests.get(url)
 
-    if response.status_code == 200:
+    if response.status_code == 200: # the good response code
         return json.loads(response.content.decode('utf-8'))
         # return response.content.decode('utf-8')
     else:
@@ -36,5 +32,18 @@ for key, value in music.items():
         # keys go from 0-999 and have a "metadata" and a "[]" at the end, these are different ignore them
         permlinks.append(music[key]['permlink'])
 
-print(permlinks)
+print(permlinks) # list of all the permlinks we will be scraping pdfs from
+
+# relevant html:
+# <a rel="nofollow" class="external text" href="https://imslp.org/wiki/Special:ImageFromIndex/693320/hfpn"> == $0
+
+def get_pdf():
+    url = permlinks[0]
+    page = requests.get(url)
+
+    soup = BeautifulSoup(page.content, "html.parser")
+    # print(soup.text)
+
+get_pdf()
+
 
