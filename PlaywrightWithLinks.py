475ffb9442b9e9a9cd09ea5c1853692b752adeef
Sadfahlsdj <124646333+Sadfahlsdj@users.noreply.github.com>
2024-01-16 01:03:21 +0000

    random changes so i can work in class tomorrow with updated files

--- PlaywrightWithLinks.py
+++ PlaywrightWithLinks.py
@@ -10,6 +10,7 @@ def run(playwright: Playwright) -> None:
     page = browser.new_page()
     # request_context = context.request
     page.goto("https://imslp.org/wiki/Special:IMSLPImageHandler/43455")
+    page.get_by_role("link", name="I accept this disclaimer,")
     print("step 1")
     # page.get_by_text("Accept all & visit the site").click()
     # print("step 2")
@@ -19,10 +20,15 @@ def run(playwright: Playwright) -> None:
     pdfresponses = pdfsoup.find(id="wiki-body")
 
     links = pdfresponses.find_all('a', href=True)
+    print(links)
     final_link = "https://imslp.org" + links[0]['href']
 
+    print(final_link)
+
+    response = requests.get(final_link)
+
     with open("testpdf.pdf", "wb") as p:
-        p.write()
+        p.write(response.content)
     # page.get_by_role("link", name="Click here to continue your").click()
     print("step 2")
 
