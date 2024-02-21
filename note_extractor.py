4b1ab12a8f5650c7da885b06915bc4a0d9ca3e0e
Sadfahlsdj's mac <124646333+Sadfahlsdj@users.noreply.github.com>
2024-02-21 11:27:21 +0000

    note extractor works, very basic form of one though

--- note_extractor.py
+++ note_extractor.py
@@ -1,11 +1,11 @@
 import music21
+import os
 
 # duration is measured in multiples of quarter notes
 # documentation: https://web.mit.edu/music21/doc/usersGuide/index.html
 
 
-s = music21.converter.parse('./mxls/pdf0.mvt1.mxl')
-test = s
+s = music21.converter.parse('./mxls/pdf12517.mvt27.mxl')
 # test.plot('horizontalbar')
 
 # test.show('text')
@@ -15,14 +15,28 @@ test = s
     print(thisNote, thisNote.offset)"""
 # above code block will skip chords
 
-for tN in test.recurse().notes: # .recurse() is needed, idk why
-    # .notes is equal to getting only notes & chords
-    if isinstance(tN, music21.chord.Chord):
-        for p in tN.pitches:
-            print(f"chord pitch: {p}")
-    else:
-        # print([tN.step, tN.octave, tN.duration.quarterLength])
-        print(tN.pitch)
+def extract_notes(music_stream):
+    note_list = []
+    for tN in music_stream.recurse().notes: # .recurse() is needed, idk why
+        # .notes is equal to getting only notes & chords
+        if isinstance(tN, music21.chord.Chord):
+            # print('found chord')
+            for p in tN.pitches:
+                # print(p)
+                note_list.append(p)
+        else:
+            # print([tN.step, tN.octave, tN.duration.quarterLength])
+            # print(tN.pitch)
+            note_list.append(tN.pitch)
+    print('finished song')
+    with open('notes.txt', 'a+') as note_file:
+        for n in note_list:
+            note_file.write(str(n) + ' ')
+        note_file.write('\n')
+
+for f in os.listdir('./mxls'):
+    s = music21.converter.parse('./mxls/' + f.strip())
+    extract_notes(s)
 
 
 
